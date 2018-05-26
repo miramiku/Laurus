af4a2bd0699bda1c2f4726867a0a5c40756bf41d
miramiku <miramiku@gmail.com>
2018-05-26 21:49:27 +0900

    modified slider

--- features/laurus.js
+++ features/laurus.js
@@ -2371,7 +2371,7 @@ LAURUS.advisor = ( function () {
 			$( "#wear-accessories" )
 				.ionRangeSlider( {
 					"values": [
-						"≧3", "4", "5", "6",
+						"≦3", "4", "5", "6",
 						"7", "8", "9", "10",
 						"11", "12", "13", "14",
 						"15", "16≦"
